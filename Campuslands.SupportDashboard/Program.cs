2aacd3b9f5dd2f16ea23ec7c54adf59a4a4bab08
ManuelDaza16 <dazacmalejo@gmail.com>
2025-10-22 10:22:39 -0500

    Creo el servicio TicketService y el archivo tickets.json para simular la API

--- Campuslands.SupportDashboard/Program.cs
+++ Campuslands.SupportDashboard/Program.cs
@@ -1,10 +1,13 @@
 using Campuslands.SupportDashboard.Components;
+using Campuslands.SupportDashboard.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<TicketService>();
 
 var app = builder.Build();
 
---
+++ Campuslands.SupportDashboard/Services/TicketService.cs
@@ -0,0 +1,32 @@
+﻿using Campuslands.SupportDashboard.Models;
+
+namespace Campuslands.SupportDashboard.Services
+{
+    public class TicketService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TicketService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Ticket>> GetTicketsAsync()
+        {
+            var tickets = await _httpClient.GetFromJsonAsync<List<Ticket>>("tickets.json");
+            return tickets ?? new List<Ticket>();
+        }
+
+        public async Task<Ticket?> GetTicketByIdAsync(int id)
+        {
+            var tickets = await GetTicketsAsync();
+            return tickets.FirstOrDefault(t => t.Id == id);
+        }
+
+        public async Task UpdateStatusAsync(int id, string newStatus)
+        {
+            await Task.Delay(200);
+            Console.WriteLine($"[Simulación PATCH] Ticket {id} actualizado a estado: {newStatus}");
+        }
+    }
+}
